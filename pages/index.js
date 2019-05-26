264292dd672bf632b65135cb3ceeb46615849017
Coston Perkins <costonperkins@me.com>
2019-05-26 12:24:12 -0500

    Set background to deeper robin's egg blue

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ class App extends React.Component {
         <style>{`
             html {
                 min-height: 100%;
-                background: rgba(0,255,255,.6);
+                background: rgba(0,255,255,.7);
             }
             a {
                 color: #000;
