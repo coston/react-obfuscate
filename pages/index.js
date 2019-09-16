a28972b03f1535609405bae8c8b46b03d6082929
Coston Perkins <costonperkins@me.com>
2019-09-16 14:46:54 -0500

    style: change demo background

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ class App extends React.Component {
         <style>{`
             html {
                 min-height: 100%;
-                background: rgba(0,255,255,.7);
+                background: #fc929e;
             }
             a {
                 color: #000;
