ee6009af7f4ce60eeec995674876e4bbf4867a0d
Coston Perkins <costonperkins@me.com>
2024-04-17 12:38:29 -0500

    test: use react-testing-library

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.{js,jsx}'],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jest-environment-jsdom',
+};
