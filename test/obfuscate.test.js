6d8d63da7dcfcb49506825db75c5bb05d6a32408
Coston Perkins <costonperkins@me.com>
2024-04-17 12:50:25 -0500

    test: use react-testing-library (#358)

--- test/obfuscate.test.js
+++ test/obfuscate.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-
+import { render, fireEvent } from '@testing-library/react';
 import Obfuscate from '../src/obfuscate';
 
 const testEmail = 'coston.perkins@ua.edu';
 const testTel = '205-454-1234';
-const testTelReveresed = '4321-454-502';
+const testHref = 'https://test-href.com/';
+const testTelReversed = '4321-454-502';
 const originalLocation = 'https://example.com/';
 
 describe('obfuscate', () => {
@@ -18,15 +18,15 @@ describe('obfuscate', () => {
   });
 
   test('renders an obfuscated href', () => {
-    const wrapper = shallow(<Obfuscate tel={testTel} />);
+    const { getByRole } = render(<Obfuscate tel={testTel} />);
 
-    expect(wrapper.prop('href')).toEqual('obfuscated');
+    expect(getByRole('link')).toHaveAttribute('href', 'obfuscated');
   });
 
   test('properly sets location.href when obfuscated email is clicked', () => {
-    const wrapper = shallow(<Obfuscate email={testEmail} />);
+    const { getByRole } = render(<Obfuscate email={testEmail} />);
 
-    wrapper.simulate('click', { preventDefault: () => {} });
+    fireEvent.click(getByRole('link'));
     expect(global.window.location.href).toEqual(`mailto:${testEmail}`);
   });
 
@@ -37,156 +37,162 @@ describe('obfuscate', () => {
       subject: 'react-obfuscate',
       body: 'Down with the machines!',
     };
-    const wrapper = shallow(<Obfuscate email={testEmail} headers={headers} />);
+    const { getByRole } = render(
+      <Obfuscate email={testEmail} headers={headers} />
+    );
 
-    wrapper.simulate('click', { preventDefault: () => {} });
+    fireEvent.click(getByRole('link'));
     expect(global.window.location.href).toEqual(
       `mailto:${testEmail}?${Object.keys(headers)
         .map((key) => `${key}=${encodeURIComponent(headers[key])}`)
         .join('&')}`
     );
   });
-
   test('properly sets location.href when obfuscated tel is clicked', () => {
-    const wrapper = shallow(<Obfuscate tel={testTel} />);
+    const { getByRole } = render(<Obfuscate tel={testTel} />);
 
-    wrapper.simulate('click', { preventDefault: () => {} });
+    fireEvent.click(getByRole('link'));
     expect(global.window.location.href).toEqual(`tel:${testTel}`);
   });
 
   test('properly sets location.href when obfuscated href is clicked', () => {
-    const wrapper = shallow(<Obfuscate href={testTel} />);
+    const { getByRole } = render(<Obfuscate href={testHref} />);
 
-    wrapper.simulate('click', { preventDefault: () => {} });
-    expect(global.window.location.href).toEqual(testTel);
+    fireEvent.click(getByRole('link'));
+    expect(global.window.location.href).toEqual(testHref);
   });
 
   test('properly sets location.href when obfuscated sms is clicked', () => {
-    const wrapper = shallow(<Obfuscate sms={testTel} />);
+    const { getByRole } = render(<Obfuscate sms={testTel} />);
 
-    wrapper.simulate('click', { preventDefault: () => {} });
+    fireEvent.click(getByRole('link'));
     expect(global.window.location.href).toEqual(`sms:${testTel}`);
   });
 
   test('properly sets location.href when obfuscated facetime is clicked', () => {
-    const wrapper = shallow(<Obfuscate facetime={testTel} />);
+    const { getByRole } = render(<Obfuscate facetime={testTel} />);
 
-    wrapper.simulate('click', { preventDefault: () => {} });
+    fireEvent.click(getByRole('link'));
     expect(global.window.location.href).toEqual(`facetime:${testTel}`);
   });
 
-  test('properly sets location.href when obfuscated without type is clicked', () => {
-    const wrapper = shallow(<Obfuscate>test</Obfuscate>);
+  test('properly sets location.href using the child content when obfuscated without type is clicked', () => {
+    const { getByRole } = render(<Obfuscate>test</Obfuscate>);
 
-    wrapper.simulate('click', { preventDefault: () => {} });
+    fireEvent.click(getByRole('link'));
     expect(global.window.location.href).toEqual('test');
   });
 
   test('renders an unobfuscated href when obfuscate prop equals false', () => {
-    const wrapper = shallow(<Obfuscate obfuscate={false} sms={testTel} />);
+    const { getByRole } = render(<Obfuscate obfuscate={false} sms={testTel} />);
 
-    expect(wrapper.find('a').text()).toBe(testTel);
-    expect(wrapper.prop('href')).toEqual(`sms:${testTel}`);
+    expect(getByRole('link')).toHaveAttribute('href', `sms:${testTel}`);
   });
 
   test('renders an unobfuscated child element left to right when obfuscate prop equals false', () => {
-    const wrapper = shallow(<Obfuscate obfuscate={false} facetime={testTel} />);
+    const { getByRole } = render(
+      <Obfuscate obfuscate={false} facetime={testTel} />
+    );
 
-    expect(wrapper.find('a').text()).toBe(testTel);
-    expect(wrapper.prop('href')).toEqual(`facetime:${testTel}`);
+    expect(getByRole('link')).toHaveTextContent(testTel);
+    expect(getByRole('link')).toHaveAttribute('href', `facetime:${testTel}`);
   });
 
   test('renders an unobfuscated child element right to left when obfuscated', () => {
-    const wrapper = shallow(<Obfuscate obfuscate tel={testTel} />);
+    const { getByRole } = render(<Obfuscate obfuscate tel={testTel} />);
 
-    expect(wrapper.find('a').text()).toBe(testTelReveresed);
-    expect(wrapper.prop('href')).toEqual('obfuscated');
+    expect(getByRole('link')).toHaveTextContent(testTelReversed);
+    expect(getByRole('link')).toHaveAttribute('href', 'obfuscated');
   });
 
   test('renders a custom element', () => {
-    const wrapper = shallow(
-      <Obfuscate element="span" viewOnly tel={testTel} />
+    const { getByRole } = render(
+      <Obfuscate element="span" role="link" tel={testTel} />
     );
 
-    expect(wrapper.find('span').text()).toBe(testTelReveresed);
-    expect(wrapper.prop('href')).toBeUndefined();
-    expect(wrapper.prop('onClick')).toBeUndefined();
+    expect(getByRole('link')).toHaveTextContent(testTelReversed);
+    expect(getByRole('link')).not.toHaveAttribute('href');
+    expect(getByRole('link')).not.toHaveAttribute('onClick');
   });
 
   test('calls supplied onClick method', () => {
     const onClick = jest.fn();
-    const wrapper = shallow(<Obfuscate email={testEmail} onClick={onClick} />);
+    const { getByRole } = render(
+      <Obfuscate email={testEmail} onClick={onClick} />
+    );
 
-    wrapper.simulate('mouseover');
-    wrapper.simulate('click', { preventDefault: () => {} });
+    fireEvent.mouseOver(getByRole('link'));
+    fireEvent.click(getByRole('link'));
     expect(onClick).toHaveBeenCalled();
-    expect(wrapper.prop('href')).toEqual(`mailto:${testEmail}`);
+    expect(getByRole('link')).toHaveAttribute('href', `mailto:${testEmail}`);
   });
 
   test('unobfuscates href when user mouses over element', () => {
-    const wrapper = shallow(<Obfuscate email={testEmail} />);
+    const { getByRole } = render(<Obfuscate email={testEmail} />);
 
-    wrapper.simulate('mouseover');
-    expect(wrapper.prop('href')).toEqual(`mailto:${testEmail}`);
+    fireEvent.mouseOver(getByRole('link'));
+    expect(getByRole('link')).toHaveAttribute('href', `mailto:${testEmail}`);
   });
 
   test('unobfuscates href when user focuses over element', () => {
-    const wrapper = shallow(<Obfuscate email={testEmail} />);
+    const { getByRole } = render(<Obfuscate email={testEmail} />);
 
-    wrapper.simulate('focus');
-    expect(wrapper.prop('href')).toEqual(`mailto:${testEmail}`);
+    fireEvent.focus(getByRole('link'));
+    expect(getByRole('link')).toHaveAttribute('href', `mailto:${testEmail}`);
   });
 
   test('unobfuscates href when user opens context menu over element', () => {
-    const wrapper = shallow(<Obfuscate email={testEmail} />);
+    const { getByRole } = render(<Obfuscate email={testEmail} />);
 
-    wrapper.simulate('contextmenu');
-    expect(wrapper.prop('href')).toEqual(`mailto:${testEmail}`);
+    fireEvent.contextMenu(getByRole('link'));
+    expect(getByRole('link')).toHaveAttribute('href', `mailto:${testEmail}`);
   });
 
   test('Human interaction disables onClick setting js location.href', () => {
-    const wrapper = shallow(<Obfuscate facetime={testTel} />);
-    wrapper.simulate('mouseover');
-    wrapper.simulate('click');
+    const { getByRole } = render(<Obfuscate facetime={testTel} />);
+
+    fireEvent.mouseOver(getByRole('link'));
+    fireEvent.click(getByRole('link'));
     expect(global.window.location.href.toString()).toEqual(originalLocation);
   });
 
   test('Return empty href link if child is an object and no link is provided', () => {
-    const wrapper = shallow(
+    const { getByRole } = render(
       <Obfuscate>
         <button type="button">This is a child object</button>
       </Obfuscate>
     );
 
-    wrapper.simulate('mouseover');
-    expect(wrapper.prop('href')).toEqual('');
+    fireEvent.mouseOver(getByRole('button'));
+    expect(getByRole('button').closest('a')).toHaveAttribute('href', '');
   });
 
   test('Undefined does not break reversal', () => {
-    const wrapper = shallow(<Obfuscate />);
-    wrapper.simulate('mouseover');
-    expect(wrapper.prop('href')).toBeUndefined();
+    const { getByRole } = render(<Obfuscate role="link" />);
+
+    fireEvent.mouseOver(getByRole('link'));
+    expect(getByRole('link')).not.toHaveAttribute('href');
   });
 
   test('renders an unobfuscated children when obfuscateChildren is false', () => {
-    const wrapper = shallow(
+    const { getByRole } = render(
       <Obfuscate obfuscateChildren={false} tel={testTel} />
     );
 
-    expect(wrapper.find('a').text()).toBe(testTel);
-    expect(wrapper.prop('href')).toEqual('obfuscated');
-    expect(wrapper.prop('style')).toEqual({
+    expect(getByRole('link')).toHaveTextContent(testTel);
+    expect(getByRole('link')).toHaveAttribute('href', 'obfuscated');
+    expect(getByRole('link')).toHaveStyle({
       direction: 'ltr',
       unicodeBidi: 'bidi-override',
     });
   });
 
   test('includes additional style prop values', () => {
-    const wrapper = shallow(
+    const { getByRole } = render(
       <Obfuscate style={{ color: 'test' }} tel={testTel} />
     );
 
-    expect(wrapper.prop('style')).toEqual({
+    expect(getByRole('link')).toHaveStyle({
       color: 'test',
       direction: 'rtl',
       unicodeBidi: 'bidi-override',
