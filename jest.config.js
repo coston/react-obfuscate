3ffdfbbc6afbbf61a9691ff612db42e7d4bed87a
Coston Perkins <costonperkins@me.com>
2024-08-09 18:51:19 -0500

    feat(#323): typescript and module support (#359)
    * refactor: use typescript
    
    * chore: revert auto added tsconfig
    
    * ci: bump node
    
    * chore: ts configuration
    
    * style: linting
    
    * test: ts test configs
    
    * chore: update eslint
    
    * chore: ignore lib for eslint
    
    * style: linting
    
    * style: format
    
    * 3.6.10-0
    
    * chore: rename files
    
    * 3.6.10-1
    
    * chore: ts config
    
    * 3.6.10-2
    
    * chore: remove babel

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,11 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.{js,jsx}'],
-  coverageDirectory: 'coverage',
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  testEnvironment: 'jest-environment-jsdom',
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  globals: {
+    "ts-jest": {
+      tsconfig: "tsconfig.test.json",
+    },
+  },
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"], // Add this line
 };
